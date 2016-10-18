1431d3976e48171c6c3e3571f750f901441b67e4
Michael Laszlo <git@michaellaszlo.com>
2016-10-17 23:35:05 -0400

    Rewrite loader with module pattern

---
+++ skeleton.js
@@ -0,0 +1,11 @@
+var Skeleton = (function () {
+
+  function load() {
+  }
+
+  return {
+    load: load
+  };
+})();
+
+window.onload = Skeleton.load;
--- vertical-shooter.js
+++
@@ -1,5 +0,0 @@
-function VerticalShooter() {
-}
-
-var game = new VerticalShooter;
-window.onload = game.load;
