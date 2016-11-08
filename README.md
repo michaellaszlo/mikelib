09fdaa7b8053f9d43475c6082bdfb15ea82e1e4a
Michael Laszlo <git@michaellaszlo.com>
2016-11-07 20:09:10 -0500

    Demo make and makeUnselectable

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This repository contains the latest version of
 [mikelib.js](https://github.com/michaellaszlo/mikelib/blob/master/js/mikelib.js),
 a tiny library that I use for basic DOM manipulation and event handling.
 
+
--- js/demo_mikelib.js
+++ js/demo_mikelib.js
@@ -1,6 +1,12 @@
-var Skeleton = (function () {
+var Demo = (function () {
 
   function load() {
+    var wrapper = M.make('div', { id: 'wrapper', parent: document.body }),
+        display = M.make('span', { className: 'display', parent: wrapper }),
+        input;
+    M.make('br', { parent: wrapper });
+    input = M.make('input', { className: 'phrase', parent: wrapper });
+    M.makeUnselectable(document.body);
   }
 
   return {
@@ -8,4 +14,4 @@ var Skeleton = (function () {
   };
 })();
 
-window.onload = Skeleton.load;
+onload = Demo.load;
