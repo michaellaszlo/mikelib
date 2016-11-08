c31ca368270f2634a3d057ebae09535d595637ca
Michael Laszlo <git@michaellaszlo.com>
2016-11-07 20:38:59 -0500

    Add event handling to demo

--- js/demo_mikelib.js
+++ js/demo_mikelib.js
@@ -1,12 +1,50 @@
 var Demo = (function () {
+  var wrapper,
+      display,
+      input,
+      colorNames = [ 'peach', 'lemon', 'sky', 'mint' ];
+
+  function colorizeInput() {
+    var i, ch,
+        colorIndex = 0,
+        text = input.value;
+    display.innerHTML = '';
+    for (i = 0; i < text.length; ++i) {
+      ch = text.charAt(i);
+      if (ch === ' ') {
+        ch = '&nbsp;';
+      } else {
+        ++colorIndex;
+        if (colorIndex == colorNames.length) {
+          colorIndex = 0;
+        }
+      }
+      M.make('span', {
+        parent: display,
+        innerHTML: ch,
+        className: colorNames[colorIndex]
+      });
+    }
+  }
 
   function load() {
-    var wrapper = M.make('div', { id: 'wrapper', parent: document.body }),
-        display = M.make('span', { className: 'display', parent: wrapper }),
-        input;
+    // Add elements to the page.
+    wrapper = M.make('div', { id: 'wrapper', parent: document.body }),
+    display = M.make('span', { className: 'display', parent: wrapper }),
     M.make('br', { parent: wrapper });
     input = M.make('input', { className: 'phrase', parent: wrapper });
+
+    // Make the body unselectable.
     M.makeUnselectable(document.body);
+
+    // Add event handlers to the input field.
+    [ 'change', 'keydown', 'keyup' ].forEach(function (eventName) {
+      input.addEventListener(eventName, colorizeInput);
+    });
+    input.value = 'The quick brown fox jumps ';
+    colorizeInput();
+    input.setSelectionRange(input.value.length, input.value.length);
+    input.focus();
   }
 
   return {
